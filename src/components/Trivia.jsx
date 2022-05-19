4848c9881ee4620d6f918353a2b8a662e92bbf4d
Ilfir Yakupov <philsmirnoff@gmail.com>
2022-05-19 00:21:55 -0400

    set the quizz app logics

--- src/components/Trivia.jsx
+++ src/components/Trivia.jsx
@@ -1,16 +1,30 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
-function Trivia() {
+function Trivia({
+  data,
+  setTimeOut,
+  questionNumber,
+  setQuestionNumber,}) {
+    const [question, setQuestion] = useState(null);
+    const [selectedAnswer, setSelectedAnswer] = useState(null);
+    const [className, setClassName] = useState("answer");
+
+    const handleClick = (e) => {
+      setSelectedAnswer(e);
+      setClassName("answer active")
+    }
+    useEffect(() => {
+      setQuestion(data[questionNumber-1]);
+    }, [data, questionNumber]);
   return (
     <div className='trivia'>
       <div className="question">
-        Whats the best youtube channel?
+       {question?.question}
       </div>
       <div className="answers">
-        <div className="answer wrong">Lama Dev</div>
-        <div className="answer">Lama Dev</div>
-        <div className="answer">Lama Dev</div>
-        <div className="answer">Lama Dev</div>
+       {question?.answers.map((a) => (
+          <div className={selectedAnswer === a ? className : "answer"} onClick={() => handleClick(a)}>{a.text}</div>
+       ))}
         </div>
     </div>
   )
